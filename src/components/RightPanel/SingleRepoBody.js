b966622f4551b6a348b924c12602d4b21a83f3eb
rohit-0308 <rohitkumar860311@gmail.com>
2021-10-17 17:04:00 +0530

    Few Bug Fixed

--- src/components/RightPanel/SingleRepoBody.js
+++ src/components/RightPanel/SingleRepoBody.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import Branches from "./Branches";
 import Issues from "./Issues";
